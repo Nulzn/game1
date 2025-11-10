2667df3585ba1ac6a7b6ba56ace670d3f5f8e0dd
MrWuk <simon.zaimon@gmail.com>
2025-11-10 21:11:20 +0100

    Changes in Enemy

--- src/entities/Enemy.py
+++ src/entities/Enemy.py
@@ -1,5 +1,4 @@
 import pygame as pg
-from pygame import *
 import math
 import random
 
@@ -14,7 +13,6 @@ class Enemy (pg.sprite.Sprite):
         self.image = pg.transform.scale(self.original_image, (self.size, self.size))
         self.pos = pg.Vector2(300,300)
     
-        
         self.rect = self.image.get_rect(center=self.pos)
         
         self.speed = 25
@@ -23,11 +21,9 @@ class Enemy (pg.sprite.Sprite):
 
         
 
-    def spawn(self, player_pos, enemy_speed=25):
-        
+    def spawn(self, player_pos):
         angle = random.randint(0,359)
         angle_to_rad = angle * math.pi / 180.0
-
 
         spawn_x = math.cos(angle_to_rad)
         spawn_y = math.sin(angle_to_rad)
@@ -36,9 +32,7 @@ class Enemy (pg.sprite.Sprite):
         spawn_far = 500
         distance = random.randint(spawn_close, spawn_far)
 
-        #h = pg.Vector2(p_x,p_y)
         self.pos = player_pos + pg.Vector2(distance*spawn_x, distance*spawn_y)
-        self.speed = enemy_speed
 
 
     def update(self, delta, player_pos):        
@@ -47,16 +41,3 @@ class Enemy (pg.sprite.Sprite):
             direction = direction.normalize()
             self.pos += direction*self.speed*delta
             self.rect.center = self.pos
-
-    def deal_dmg(self, dmg_amount):
-        # returns true if enemy died
-        self.hp -= dmg_amount
-        return self.hp <= 0
-
-    ## This only check circle collisions
-    def check_collision_with(self, object_pos, object_radius):
-        return self.pos.distance_to(object_pos) < (object_radius+self.size)
-
-
-    def draw(self, screen):
-        pg.draw.circle(screen,"red", self.pos, self.size)
